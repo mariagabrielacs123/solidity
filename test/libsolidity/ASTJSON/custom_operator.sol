739b3209edff25da7355217249b5dffe7ba1e0fa
wechman <damian.wechman.it@gmail.com>
2022-09-23 13:13:22 +0200

    Simplify ASTJSON test for custom operators.

--- test/libsolidity/ASTJSON/custom_operator.sol
+++ test/libsolidity/ASTJSON/custom_operator.sol
@@ -1,12 +1,8 @@
 type UInt is uint8;
 using {add as +} for UInt;
-function add(UInt, UInt) pure returns (UInt) {
-    return UInt.wrap(0);
-}
-contract C {
-    function f() public pure {
-        UInt.wrap(0) + UInt.wrap(1);
-    }
-}
 
-// ----
+function add(UInt, UInt) pure returns (UInt) {}
+
+function f() pure {
+    UInt.wrap(0) + UInt.wrap(1);
+}
